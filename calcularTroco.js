1ee5194fc5570445090676a73ca210896b4e7773
José Walisson <102293307+JoseWalisson@users.noreply.github.com>
2024-06-02 21:09:22 -0300

    Limpando valores automaticamente

--- calcularTroco.js
+++ calcularTroco.js
@@ -8,11 +8,17 @@ function calcNewValue() {
     
      if (newValueProduct === newValuePaid) {
         window.alert("Você não receberá nada de troco!");
+        paid.value = "";
+        productValue.value = "";
     } else if (newValuePaid < newValueProduct) {
         window.alert("O valor precisa ser maior para se calcular o troco!");
+        paid.value = "";
+        productValue.value = "";
     } else {
         const resultado = newValuePaid - newValueProduct;
         window.alert(`O produto custou R$ ${newValueProduct} e você receberá R$ ${resultado} de troco!`);
+        paid.value = "";
+        productValue.value = "";
     }
 }
 
